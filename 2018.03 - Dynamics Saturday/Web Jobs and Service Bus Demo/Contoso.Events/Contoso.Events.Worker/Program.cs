ca440125a9f946bbb5421a5698d5515fcc69e18c
Tiago Costa <tiago.costa@outlook.com>
2018-03-19 13:53:42 +0000

    Upload Web Jobs and Service Bus Demo
    Upload Web Jobs and Service Bus Demo from Dynamics Saturday Lisbon 2018.

---
+++ 2018.03 - Dynamics Saturday/Web Jobs and Service Bus Demo/Contoso.Events/Contoso.Events.Worker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace Contoso.Events.Worker
+{
+    class Program
+    {
+        static void Main()
+        {
+            JobHostConfiguration config = new JobHostConfiguration();
+            config.UseServiceBus();
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
